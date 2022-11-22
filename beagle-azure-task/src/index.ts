a9ec3251ce67cd44a2228617f02795ea592c64e4
aadheeb <adheeb.a@beaglesecurity.com>
2022-11-22 10:45:57 +0530

    Updated api from v1 to v2

--- beagle-azure-task/src/index.ts
+++ beagle-azure-task/src/index.ts
@@ -1,20 +1,25 @@
-import tl = require('azure-pipelines-task-lib/task');
+import tl = require('azure-pipelines-task-lib/task')
+
 
 async function run() {
     try {
-        const app_key: string | undefined = tl.getInput('applicationkey', true);
-        const access_key: string | undefined = tl.getInput('accesskey', true);
-        var request = require("request")
+        const app_key: string | undefined = tl.getInput('applicationToken', true);
+        const access_key: string | undefined = tl.getInput('Authorization', true);
+        var request = require("request");
         var requestData = {
-            "access_token": access_key,
-            "application_token": app_key 
+            "applicationToken": app_key
         }
-        var api = "https://api.beaglesecurity.com/v1/test/start";
+        var api = "https://api.beaglesecurity.com/rest/v2/test/start/";
         request({
             url: api,
             method: "POST",
-            json: requestData
-        }, function (error: string, response: { statusCode: string | number; statusText: string; }, body: any) {
+            json: requestData,
+            headers: {
+                'Authorization': "Bearer "+access_key
+            }
+        }, 
+        function (error: string, response: { statusCode: string | number; statusText: string; }, body: any) {
+            console.log(response)
             if (!error && response.statusCode === 200) {
                 console.log(body)
             }
@@ -25,7 +30,7 @@ async function run() {
         });
     }
     catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+         tl.setResult(tl.TaskResult.Failed, (err as Error).message);
     }
 }
 
